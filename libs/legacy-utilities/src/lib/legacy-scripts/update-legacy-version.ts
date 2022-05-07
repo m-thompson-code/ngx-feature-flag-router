14c96e4487a73283b4033fd203f78656d5fd50a2
Mark Thompson <mthompson@bitovi.com>
2022-05-06 19:04:13 -0700

    Added writing updates to each package.json

--- libs/legacy-utilities/src/lib/legacy-scripts/update-legacy-version.ts
+++ libs/legacy-utilities/src/lib/legacy-scripts/update-legacy-version.ts
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-
 import * as fs from 'fs-extra';
 
 import { AngularVersion } from '../types';
@@ -12,8 +10,6 @@ import {
 } from '../utilities';
 
 export const updateLegacyPackageJson = (angularVersion: AngularVersion): void => {
-    console.log(getProjectPackageJsonPath());
-
     const sourcePackage = fs.readJsonSync(getProjectPackageJsonPath());
 
     const { version: sourceVersion, description, license, repository, bugs, homepage, author, keywords } = sourcePackage;
@@ -37,29 +33,7 @@ export const updateLegacyPackageJson = (angularVersion: AngularVersion): void =>
         keywords,
     };
 
-    console.log(legacyLibPackageJsonPath);
-    console.log(legacyLibPackage);
-
-    fs.writeJSONSync(legacyLibPackageJsonPath, legacyLibPackage);
+    fs.writeJSONSync(legacyLibPackageJsonPath, legacyLibPackage, {
+        spaces: 4,
+    });
 }
-
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/package-lock.json
-
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-9/package-lock.json
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-9/package.json
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-9/projects/ngx-feature-flag-router/package.json
-
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-10/package-lock.json
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-10/package.json
-
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-10/projects/ngx-feature-flag-router/package.json
-
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-11/package-lock.json
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-11/package.json
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-11/projects/ngx-feature-flag-router/package.json
-
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-12/package-lock.json
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-12/package.json
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/apps/legacy/angular-12/projects/ngx-feature-flag-router/package.json
-
-// /Users/markthompson/Documents/github/personal/ngx-feature-flag-router/libs/ngx-feature-flag-router/package.json
